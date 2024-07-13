cfe6f2440426dbd16edaec30b597aaa396aadbdf
Adnane Saber <58700213+AdnaneSaber@users.noreply.github.com>
2024-07-14 06:29:26 +0900

    Update index.py

--- api/index.py
+++ api/index.py
@@ -5,4 +5,4 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     file_path = "../3S_PC_automation.exe"
-    return send_file(file_path, as_attachment=True, attachment_filename='3S_PC_automation.exe')
+    return send_file(file_path, as_attachment=True, download_name='3S_PC_automation.exe')
