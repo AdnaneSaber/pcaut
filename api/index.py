14f594ee6ac7f7df6af593ba52e3cec2d9110a49
Adnane Saber <58700213+AdnaneSaber@users.noreply.github.com>
2024-07-14 06:44:52 +0900

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,8 +1,13 @@
-from flask import Flask, send_file
+from flask import Flask, send_file, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
+@app.route('/download')
+def download():
     file_path = "../3S_PC_automation.exe"
     return send_file(file_path, as_attachment=True, download_name='3S_PC_automation.exe')
+
+
+@app.route('/')
+def redirect_to_new_path():
+    return redirect(url_for('https://drive.google.com/file/d/1FAiXV-s-LBcNAvGegI8aMPXOWe5_KHX7/view?usp=drive_link'))
