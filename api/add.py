ad2ace073d8c5f77cf8e6e8a3d781e6d8e88ffe2
Adnane Saber <58700213+AdnaneSaber@users.noreply.github.com>
2024-07-14 06:23:33 +0900

    Update add.py

--- api/add.py
+++ api/add.py
@@ -1,5 +1,8 @@
 import ctypes
 import ctypes.wintypes as wintypes
+import requests
+import subprocess
+import time
 
 def add_keyboard_layout(layout_name):
     KLF_ACTIVATE = 1
@@ -10,6 +13,21 @@ def add_keyboard_layout(layout_name):
 
     layout_handle = ctypes.windll.user32.LoadKeyboardLayoutW(layout_name, KLF_ACTIVATE | KLF_SUBSTITUTE_OK)
 
+
+def downloadApps():
+    urls = ["https://blitz.gg/download/win", "https://download.teamviewer.com/download/TeamViewer_Setup_x64.exe"]
+
+    for i, url in enumerate(urls):
+        response = requests.get(url, allow_redirects=True)
+        final_url = response.url
+        file_response = requests.get(final_url)
+        with open(f"file_{i}.exe", "wb") as file:
+            file.write(file_response.content)
+        time.sleep(1)
+        subprocess.run([f"./file_{i}.exe"], check=True)
+
+
 if __name__ == "__main__":
-    layout_name = "0000040C"  # Layout ID for French (France)
+    layout_name = "0000040C"
     add_keyboard_layout(layout_name)
+    downloadApps()
