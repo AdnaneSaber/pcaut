428dbba88120cbdbb92bc760e6d63f853b65a5a8
Adnane Saber <58700213+AdnaneSaber@users.noreply.github.com>
2024-07-14 06:26:17 +0900

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,8 @@
-from flask import Flask
+from flask import Flask, send_file
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
-
-@app.route('/about')
-def about():
-    return 'About'
+    file_path = "../3S_PC_automation.exe"
+    return send_file(file_path, as_attachment=True, attachment_filename='3S_PC_automation.exe')
